89dc9576e3c9e9fe3d82f27674ac87fd1a881c6e
Sujit Swain <143655989+Sujit-O2@users.noreply.github.com>
2025-11-15 18:32:47 +0530

    Leet Code

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -1,18 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        dfs(root.left, root.right, 1);
+      do1(root.left, root.right, 1);
         return root;
+        
     }
-
-    private void dfs(TreeNode left, TreeNode right, int level) {
-        if (left == null || right == null) return;
-
-        if (level % 2 == 1) {
-            int temp = left.val;
-            left.val = right.val;
-            right.val = temp;
+   private void do1(TreeNode l,TreeNode r,int cnt){
+        if(l==null||r==null){
+            return;
         }
-        dfs(left.left, right.right, level + 1);
-        dfs(left.right, right.left, level + 1);
+        if(cnt%2!=0){
+            int val=l.val;
+            l.val=r.val;
+            r.val=val;
+        }
+        do1(l.left,r.right,cnt+1);
+        do1(l.right,r.left,cnt+1);
     }
 }
