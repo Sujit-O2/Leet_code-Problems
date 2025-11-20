4aa0b9f7726904e4152071bdbdc1e59ae7a673aa
Sujit Swain <143655989+Sujit-O2@users.noreply.github.com>
2025-11-20 23:14:01 +0530

    Leet Code

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int alt=0;
+        int maxalt=0;
+        for(int i=0;i<gain.length;i++){
+            alt+=gain[i];
+            maxalt=Math.max(alt,maxalt);
+        }
+        return maxalt;
+        
+    }
+}
