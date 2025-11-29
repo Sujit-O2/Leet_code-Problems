0a7436fe9f82256f23a74d18dc1a58cc52711823
Sujit Swain <143655989+Sujit-O2@users.noreply.github.com>
2025-11-29 09:51:59 +0530

    Leet Code

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.java
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.java
@@ -1,12 +1,9 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
         int sum=0;
-        for(int i: nums){
-            sum+=i;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
 
-        }
-        if(sum%k==0){
-            return 0;
         }
         return sum%k;
         
