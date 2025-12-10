c976873d3c35ac667d98a754e1862f59a0cead53
Sujit Swain <143655989+Sujit-O2@users.noreply.github.com>
2025-12-10 20:38:29 +0530

    Leet Code

---
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countPermutations(int[] complexity) {
+        int n = complexity.length;
+        long MOD = 1_000_000_007;
+        for (int i = 1; i < n; i++) {
+            if (complexity[i] <= complexity[0]) {
+                return 0;
+            }
+        }
+
+        long ans = 1;
+        for (int i = 1; i < n; i++) {
+            ans = (ans * i) % MOD;
+        }
+
+        return (int) ans;
+    }
+}
