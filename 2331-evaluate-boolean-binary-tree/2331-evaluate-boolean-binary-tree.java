6bfc2eb44d322ea3e7568c3285ab582e022ce3f3
Sujit Swain <143655989+Sujit-O2@users.noreply.github.com>
2025-12-17 23:37:20 +0530

    Leet Code

---
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        // Leaf node
+        if (root.left == null && root.right == null) {
+            return root.val == 1;
+        }
+
+        boolean left = evaluateTree(root.left);
+        boolean right = evaluateTree(root.right);
+
+        if (root.val == 2) { 
+            return left || right;
+        } else {
+            return left && right;
+        }
+    }
+}
