ac08d73168c1189b2f5204318e749127cfb3d9fd
Sujit Swain <143655989+Sujit-O2@users.noreply.github.com>
2026-01-01 08:15:25 +0530

    Leet Code

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+       int n= digits.length;
+for(int i=n-1;i>-1;i--){
+    if(digits[i]<9){
+        digits[i]++;
+        return digits;
+    }
+    digits[i]=0;
+
+}
+int as[]=new int [n+1];
+as[0]=1;
+        return as;
+        
+       
+        
+    }
+}
