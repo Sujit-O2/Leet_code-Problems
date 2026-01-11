a0a86df907ce57b1665f43830e465cbcfb351597
Sujit Swain <143655989+Sujit-O2@users.noreply.github.com>
2026-01-11 22:33:18 +0530

    Leet Code

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        int leftDepth = getDepth(root.left);
+        int rightDepth = getDepth(root.right);
+        
+        if (leftDepth == rightDepth) {
+            return root;
+        } else if (leftDepth > rightDepth) {
+            return lcaDeepestLeaves(root.left);
+        } else {
+            return lcaDeepestLeaves(root.right);
+        }
+    }
+        int getDepth(TreeNode node) {
+            if (node == null) {
+                return 0;
+            }
+            return 1 + Math.max(getDepth(node.left), getDepth(node.right));
+
+
+    }
+}
