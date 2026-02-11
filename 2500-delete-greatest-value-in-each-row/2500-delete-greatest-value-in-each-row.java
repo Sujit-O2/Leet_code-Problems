ab0665101e432c691f756f29da975ba66dfc4c25
Sujit Swain <143655989+Sujit-O2@users.noreply.github.com>
2026-02-11 22:51:37 +0530

    Leet Code

---
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int deleteGreatestValue(int[][] grid) {
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        for(int i = 0; i < m; i++){
+            Arrays.sort(grid[i]);
+        }
+        
+        int sum = 0;
+        
+        for(int col = n - 1; col >= 0; col--){
+            
+            int max = 0;
+            
+            for(int row = 0; row < m; row++){
+                max = Math.max(max, grid[row][col]);
+            }
+            
+            sum += max;
+        }
+        
+        return sum;
+    }
+}
