a0f4a0c2236320cad3dcca05d78517c99ea87d60
Sujit Swain <143655989+Sujit-O2@users.noreply.github.com>
2026-02-15 23:25:32 +0530

    Leet Code

---
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        if(n < 4) return false;
+
+        int i = 0;
+
+        while(i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+
+        if(i == 0) return false;
+
+        int peak = i;
+        while(i + 1 < n && nums[i] > nums[i + 1]) {
+            i++;
+        }
+
+        if(i == peak) return false;
+        int j=i;
+
+        while(i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+        if(j==i) return  false;
+
+        return i == n - 1;
+    }
+}
