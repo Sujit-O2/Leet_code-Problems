5bcdf3330af5baaa7122791e25e8977d2bcac70d
Sujit Swain <143655989+Sujit-O2@users.noreply.github.com>
2026-02-16 00:13:38 +0530

    Leet Code

--- 3324-find-the-sequence-of-strings-appeared-on-the-screen/3324-find-the-sequence-of-strings-appeared-on-the-screen.java
+++ 3324-find-the-sequence-of-strings-appeared-on-the-screen/3324-find-the-sequence-of-strings-appeared-on-the-screen.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<String> stringSequence(String target) {
-        List<String> ss = new ArrayList<>();
+        List<String> ss=new ArrayList();
+        for(int i=0;i<target.length();i++){
+            int size =ss.size();
+            for(int j=0;j<=target.charAt(i)-'a';j++){
+                if(i==0){
+                    ss.add(""+(char)('a'+j));
 
-        for (int i = 0; i < target.length(); i++) {
-
-            int size = ss.size();
-            for (int j = 0; j <= target.charAt(i) - 'a'; j++) {
-
-                if (i == 0) {
-                    ss.add("" + (char)('a' + j));
-                } else {
+                }
+                else{
                     ss.add(ss.get(size - 1) + (char)('a' + j));
                 }
             }
         }
         return ss;
+        
     }
 }
