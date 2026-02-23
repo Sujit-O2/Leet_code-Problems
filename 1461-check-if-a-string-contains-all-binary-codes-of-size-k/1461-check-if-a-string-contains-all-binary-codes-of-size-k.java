5cf480c6a263388e69356621a350111c69b5aeef
Sujit Swain <143655989+Sujit-O2@users.noreply.github.com>
2026-02-23 08:24:03 +0530

    Leet Code

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        if(s.length()<k)return false;
+        HashSet<String> ss=new HashSet();
+        for(int i=0;i<=s.length()-k;i++){
+            ss.add(s.substring(i,i+k));
+
+        }
+        return ss.size()==(1<<k);
+        
+    }
+}
